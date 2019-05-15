2ee8ca41cc4754f83f5aae128c75844a2255fa94
InSub Lee <dldlstjq1212@gmail.com>
2019-05-15 15:50:50 +0900

    메인클래스 수정

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,28 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("/view/join.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setTitle("엘리자베스 아덴 매장관리 프로그램");
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
