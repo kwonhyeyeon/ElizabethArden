2ae9bab909a8e6e349968cd288cee461cff99c10
HyeYeon Kwon <bluuudawn@gmail.com>
2019-05-17 11:05:05 +0900

    매장 등록 컨트롤러 수정

--- src/controller/StoreJoinController.java
+++ src/controller/StoreJoinController.java
@@ -2,8 +2,11 @@ package controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.ParsePosition;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,8 +17,10 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 import model.StoreJoinVO;
 
 public class StoreJoinController implements Initializable {
@@ -36,10 +41,10 @@ public class StoreJoinController implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
 
 		btnRegiste.setOnAction(event -> handlerBtnRegisteAction(event)); // 관리자 등록 이벤트
 		btnCancle.setOnAction(event -> handlerBtnCancelAction(event)); // 등록창 닫기 이벤트
+		
 	}
 
 	// 매장등록 메소드
@@ -48,34 +53,45 @@ public class StoreJoinController implements Initializable {
 		StoreJoinVO sjvo = null;
 		StoreJoinDAO sjdao = null;
 		// 등록성공여부 판단변수
-		boolean joinSucess = false;
-
-		// JoinVO에 입력받은 id, ps, name을 공백제거후 넣어준다
-		sjvo = new StoreJoinVO(txtStoreCode.getText().trim(), txtStoreAddress.getText().trim(),
-				txtStoreName.getText().trim(), Integer.parseInt(txtPassword.getText().trim()),
-				Integer.parseInt(txtStoreTel.getText().trim()));
-		// 인스턴스화
-		sjdao = new StoreJoinDAO();
+		boolean joinSuccess = false;
 
 		try {
-			// JoinDAO에서 managerRegiste메소드를 호출하여 등록후 성공여부를 확인한다.
-			joinSucess = sjdao.getStoreRegiste(sjvo);
-
-			// 등록이 성공하였을 경우
-			if (joinSucess) {
-				// 취소버튼을 호출하여 창을 닫아준다
-				handlerBtnCancelAction(event);
-			} else { // 등록실패
-
+			
+			if(txtPassword.getText().trim().length() > 4 || txtPassword.getText().trim().equals("")) {
+				// 비밀번호 크기가 4보다 크거나 공백일 때
 				txtPassword.clear(); // 비밀번호를 지워준다
 
 				Alert alert = new Alert(AlertType.WARNING);
-				alert.setTitle("패스워드, 이름 확인");
-				alert.setHeaderText("패스워드, 이름 확인 검사에 오류가 발생");
-				alert.setContentText("수고링~");
+				alert.setTitle("매장 등록 실패");
+				alert.setHeaderText("패스워드 오류");
+				alert.setContentText("패스워드는 숫자 4자리로 입력해주세요");
 				alert.showAndWait();
+			} else if(txtStoreName.getText().trim().equals("")) { // 매장명이 공백이면
+				txtPassword.clear(); // 비밀번호를 지워준다
 
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("매장 등록 실패");
+				alert.setHeaderText("매장명 미입력");
+				alert.setContentText("매장명을 입력해주세요");
+				alert.showAndWait();
+			} else if(txtStoreAddress.getText().trim().equals("")) { // 매장주소가 공백이면
+				txtPassword.clear(); // 비밀번호를 지워준다
+
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("매장 등록 실패");
+				alert.setHeaderText("매장주소 미입력");
+				alert.setContentText("매장주소를 입력해주세요");
+				alert.showAndWait();
+			} else if(txtStoreTel.getText().trim().equals("")) { // 매장번호가 공백이면
+				txtPassword.clear(); // 비밀번호를 지워준다
+
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("매장 등록 실패");
+				alert.setHeaderText("매장번호 미입력");
+				alert.setContentText("매장번호를 입력해주세요");
+				alert.showAndWait();
 			}
+			
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
@@ -84,24 +100,8 @@ public class StoreJoinController implements Initializable {
 
 	// 등록창 닫기 메소드
 	public void handlerBtnCancelAction(ActionEvent event) {
-		// TODO Auto-generated method stub
-		// 등록창을 닫고 로그인 화면을 불러와서 띄운다.
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/login.fxml"));
-
-			Parent mainView = (Parent) loader.load();
-			Scene scane = new Scene(mainView);
-			Stage mainMtage = new Stage();
-			mainMtage.setTitle("매장 로그인");
-			mainMtage.setScene(scane);
-			Stage oldStage = (Stage) btnCancle.getScene().getWindow();
-			oldStage.close();
-			mainMtage.show();
-
-		} catch (IOException e) {
-			System.out.println("오류 " + e);
-		}
-
+		Stage stage = (Stage) btnCancle.getScene().getWindow();
+		stage.close();
 	}
 
 }
