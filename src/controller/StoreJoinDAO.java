37d3fcbb3ca3a9aaaf15d925785213fdc4e28322
InSub Lee <dldlstjq1212@gmail.com>
2019-05-17 14:17:55 +0900

    StoreJoinDAO 수정

--- src/controller/StoreJoinDAO.java
+++ src/controller/StoreJoinDAO.java
@@ -11,49 +11,46 @@ import model.StoreJoinVO;
 
 public class StoreJoinDAO {
 
-	
-	
 	// 매장코드를 가져오는 메소드
-		public String getStoreSequence(StoreJoinVO svo) throws Exception {
-			String sql = "select STORE_SEQ.nextval from dual";
-			Connection con = null;
-			PreparedStatement pstmt = null;
-			ResultSet rs = null;
+	public String getStoreSequence(StoreJoinVO svo) throws Exception {
+		String sql = "select STORE_SEQ.nextval from dual";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 
-			String scode = null;
-			try {
-				// DB연동
-				con = DBUtil.getConnection();
-				// sql문을 담아줄 그릇
-				pstmt = con.prepareStatement(sql);
-				// jvo에서 변수들을 가져와서 sql문에 넣어준다.
-				rs = pstmt.executeQuery();
-				// sql을 날리고 불러온 값이 있으면 로그인결과변수 true
-				if (rs.next()) {
-					String seq = rs.getString("nextval");
-					scode = "AD" + seq;
-				}
-			} catch (SQLException e) {
-				System.out.println(e);
-			} catch (Exception e) {
-				System.out.println(e);
-			} finally {
-
-				try {
-					// 데이터베이스와의 연결에 사용되었던 오브젝트를 해제
-					if (rs != null)
-						rs.close();
-					if (pstmt != null)
-						pstmt.close();
-					if (con != null)
-						con.close();
-				} catch (SQLException e) {
-				}
+		String scode = null;
+		try {
+			// DB연동
+			con = DBUtil.getConnection();
+			// sql문을 담아줄 그릇
+			pstmt = con.prepareStatement(sql);
+			// jvo에서 변수들을 가져와서 sql문에 넣어준다.
+			rs = pstmt.executeQuery();
+			// sql을 날리고 불러온 값이 있으면 로그인결과변수 true
+			if (rs.next()) {
+				String seq = rs.getString("nextval");
+				scode = "AD" + seq;
 			}
-			return scode;
-		}
+		} catch (SQLException e) {
+			System.out.println(e);
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
 
-	
+			try {
+				// 데이터베이스와의 연결에 사용되었던 오브젝트를 해제
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+			}
+		}
+		return scode;
+	}
+
 	public boolean getStoreRegiste(StoreJoinVO svo) throws Exception {
 		// 매장등록하는 sql문
 		String sql = "insert into store values(?, ?, ?, ?, ?)";
