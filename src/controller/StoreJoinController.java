5674db7b563180e82e4b4f21e659f221027562a4
HyeYeon Kwon <bluuudawn@gmail.com>
2019-05-17 14:18:15 +0900

    매장 등록 컨트롤러 수정

--- src/controller/StoreJoinController.java
+++ src/controller/StoreJoinController.java
@@ -42,7 +42,7 @@ public class StoreJoinController implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+
 		// 매장 전화번호에 숫자만 입력되게 적용
 		txtStoreTel.textProperty().addListener(new ChangeListener<String>() {
 
@@ -50,74 +50,89 @@ public class StoreJoinController implements Initializable {
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
 				if (!newValue.matches("\\d*")) {
 					txtStoreTel.setText(newValue.replaceAll("[^\\d]", ""));
-		        }
+				}
 			}
-			
+
 		});
 
 		btnRegiste.setOnAction(event -> handlerBtnRegisteAction(event)); // 관리자 등록 이벤트
 		btnCancle.setOnAction(event -> handlerBtnCancelAction(event)); // 등록창 닫기 이벤트
-		
+
 		StoreJoinVO sjvo = null;
 		StoreJoinDAO sjdao = null;
-		
+
 		sjdao = new StoreJoinDAO();
 		try {
 			txtStoreCode.setText(sjdao.getStoreSequence(sjvo));
 		} catch (Exception e) {
 		}
-		
+
 		txtStoreCode.setEditable(false); // 매장코드 수정불가
 		txtStoreCode.setDisable(true);
 	}
 
 	// 매장등록 메소드
 	public void handlerBtnRegisteAction(ActionEvent event) {
+
 		// 인스턴스 생성
 		StoreJoinVO sjvo = null;
 		StoreJoinDAO sjdao = null;
+
 		// 등록성공여부 판단변수
 		boolean joinSuccess = false;
 
+		if (txtPassword.getText().trim().length() > 4 || txtPassword.getText().trim().equals("")) {
+			// 비밀번호 크기가 4보다 크거나 공백일 때
+			txtPassword.clear(); // 비밀번호를 지워준다
+
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("매장 등록 실패");
+			alert.setHeaderText("패스워드 오류");
+			alert.setContentText("패스워드는 숫자 4자리로 입력해주세요");
+			alert.showAndWait();
+		} else if (txtStoreName.getText().trim().equals("")) { // 매장명이 공백이면
+			txtPassword.clear(); // 비밀번호를 지워준다
+
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("매장 등록 실패");
+			alert.setHeaderText("매장명 미입력");
+			alert.setContentText("매장명을 입력해주세요");
+			alert.showAndWait();
+		} else if (txtStoreAddress.getText().trim().equals("")) { // 매장주소가 공백이면
+			txtPassword.clear(); // 비밀번호를 지워준다
+
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("매장 등록 실패");
+			alert.setHeaderText("매장주소 미입력");
+			alert.setContentText("매장주소를 입력해주세요");
+			alert.showAndWait();
+		} else if (txtStoreTel.getText().trim().equals("")) { // 매장번호가 공백이면
+			txtPassword.clear(); // 비밀번호를 지워준다
+
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("매장 등록 실패");
+			alert.setHeaderText("매장번호 미입력");
+			alert.setContentText("매장번호를 입력해주세요");
+			alert.showAndWait();
+		}
+
 		try {
-			
-			if(txtPassword.getText().trim().length() > 4 || txtPassword.getText().trim().equals("")) {
-				// 비밀번호 크기가 4보다 크거나 공백일 때
-				txtPassword.clear(); // 비밀번호를 지워준다
-
-				Alert alert = new Alert(AlertType.WARNING);
-				alert.setTitle("매장 등록 실패");
-				alert.setHeaderText("패스워드 오류");
-				alert.setContentText("패스워드는 숫자 4자리로 입력해주세요");
-				alert.showAndWait();
-			} else if(txtStoreName.getText().trim().equals("")) { // 매장명이 공백이면
-				txtPassword.clear(); // 비밀번호를 지워준다
-
-				Alert alert = new Alert(AlertType.WARNING);
-				alert.setTitle("매장 등록 실패");
-				alert.setHeaderText("매장명 미입력");
-				alert.setContentText("매장명을 입력해주세요");
-				alert.showAndWait();
-			} else if(txtStoreAddress.getText().trim().equals("")) { // 매장주소가 공백이면
-				txtPassword.clear(); // 비밀번호를 지워준다
-
-				Alert alert = new Alert(AlertType.WARNING);
-				alert.setTitle("매장 등록 실패");
-				alert.setHeaderText("매장주소 미입력");
-				alert.setContentText("매장주소를 입력해주세요");
-				alert.showAndWait();
-			} else if(txtStoreTel.getText().trim().equals("")) { // 매장번호가 공백이면
-				txtPassword.clear(); // 비밀번호를 지워준다
-
-				Alert alert = new Alert(AlertType.WARNING);
-				alert.setTitle("매장 등록 실패");
-				alert.setHeaderText("매장번호 미입력");
-				alert.setContentText("매장번호를 입력해주세요");
-				alert.showAndWait();
+			if (txtPassword.getText().trim().length() <= 4 && !(txtPassword.getText().trim().equals(""))) {
+				sjvo = new StoreJoinVO(txtStoreCode.getText().trim(), txtStoreAddress.getText().trim(),
+						txtStoreName.getText().trim(), Integer.parseInt(txtPassword.getText().trim()),
+						Integer.parseInt(txtStoreTel.getText().trim()));
 			}
-			
-		} catch (Exception e1) {
-			e1.printStackTrace();
+
+			sjdao = new StoreJoinDAO();
+
+			joinSuccess = sjdao.getStoreRegiste(sjvo);
+
+			if (joinSuccess) {
+				handlerBtnCancelAction(event);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 	}
