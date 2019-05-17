854c4fccb668c891a3cc85b1a7e0de1b88df8ebb
InSub Lee <dldlstjq1212@gmail.com>
2019-05-17 13:42:15 +0900

    StoreJoinController 수정

--- src/controller/StoreJoinController.java
+++ src/controller/StoreJoinController.java
@@ -1,29 +1,19 @@
 package controller;
 
-import java.io.IOException;
 import java.net.URL;
-import java.text.NumberFormat;
 import java.util.ResourceBundle;
 
-import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 import model.StoreJoinVO;
 
 public class StoreJoinController implements Initializable {
@@ -44,21 +34,11 @@ public class StoreJoinController implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-
-		// 매장 전화번호에 숫자만 입력되게 적용
-		txtStoreTel.textProperty().addListener(new ChangeListener<String>() {
-
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				if (!newValue.matches("\\d*")) {
-					txtStoreTel.setText(newValue.replaceAll("[^\\d]", ""));
-				}
-			}
-
-		});
+		// TODO Auto-generated method stub
 
 		btnRegiste.setOnAction(event -> handlerBtnRegisteAction(event)); // 관리자 등록 이벤트
 		btnCancle.setOnAction(event -> handlerBtnCancelAction(event)); // 등록창 닫기 이벤트
+
 		// 아이디 입력에서 엔터키 이벤트 적용
 		txtStoreCode.setOnKeyPressed(event -> handerTxtIdKeyPressed(event));
 		// 패스워드 입력에서 엔터키 이벤트 적용
@@ -71,72 +51,14 @@ public class StoreJoinController implements Initializable {
 		// 매장코드가져오기
 		StoreJoinVO sjvo = null;
 		StoreJoinDAO sjdao = null;
-
 		sjdao = new StoreJoinDAO();
 		try {
+			// 매장코드 설정(StoreJoinDAO에서 매장코드 가져오는 메소드 호출및 반환값으로 코드설정)
 			txtStoreCode.setText(sjdao.getStoreSequence(sjvo));
 		} catch (Exception e) {
 		}
 
 		txtStoreCode.setEditable(false); // 매장코드 수정불가
-		txtStoreCode.setDisable(true);
-	}
-
-	// 매장등록 메소드
-	public void handlerBtnRegisteAction(ActionEvent event) {
-		// 인스턴스 생성
-		StoreJoinVO sjvo = null;
-		StoreJoinDAO sjdao = null;
-		// 등록성공여부 판단변수
-		boolean joinSuccess = false;
-
-		try {
-
-			if (txtPassword.getText().trim().length() > 4 || txtPassword.getText().trim().equals("")) {
-				// 비밀번호 크기가 4보다 크거나 공백일 때
-				txtPassword.clear(); // 비밀번호를 지워준다
-
-				Alert alert = new Alert(AlertType.WARNING);
-				alert.setTitle("매장 등록 실패");
-				alert.setHeaderText("패스워드 오류");
-				alert.setContentText("패스워드는 숫자 4자리로 입력해주세요");
-				alert.showAndWait();
-			} else if (txtStoreName.getText().trim().equals("")) { // 매장명이 공백이면
-				txtPassword.clear(); // 비밀번호를 지워준다
-
-				Alert alert = new Alert(AlertType.WARNING);
-				alert.setTitle("매장 등록 실패");
-				alert.setHeaderText("매장명 미입력");
-				alert.setContentText("매장명을 입력해주세요");
-				alert.showAndWait();
-			} else if (txtStoreAddress.getText().trim().equals("")) { // 매장주소가 공백이면
-				txtPassword.clear(); // 비밀번호를 지워준다
-
-				Alert alert = new Alert(AlertType.WARNING);
-				alert.setTitle("매장 등록 실패");
-				alert.setHeaderText("매장주소 미입력");
-				alert.setContentText("매장주소를 입력해주세요");
-				alert.showAndWait();
-			} else if (txtStoreTel.getText().trim().equals("")) { // 매장번호가 공백이면
-				txtPassword.clear(); // 비밀번호를 지워준다
-
-				Alert alert = new Alert(AlertType.WARNING);
-				alert.setTitle("매장 등록 실패");
-				alert.setHeaderText("매장번호 미입력");
-				alert.setContentText("매장번호를 입력해주세요");
-				alert.showAndWait();
-			}
-
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
-
-	}
-
-	// 등록창 닫기 메소드
-	public void handlerBtnCancelAction(ActionEvent event) {
-		Stage stage = (Stage) btnCancle.getScene().getWindow();
-		stage.close();
 	}
 
 	// 매장주소 입력에서 엔터키 이벤트 적용
@@ -173,6 +95,82 @@ public class StoreJoinController implements Initializable {
 			// 비밀번호창으로 포커스를 준다.
 			txtPassword.requestFocus();
 		}
+	}
+
+	// 매장등록 메소드
+	public void handlerBtnRegisteAction(ActionEvent event) {
+
+		try {
+
+			if (txtPassword.getText().trim().length() > 4 || txtPassword.getText().trim().equals("")) {
+				// 비밀번호 크기가 4보다 크거나 공백일 때
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("매장 등록 실패");
+				alert.setHeaderText("패스워드 오류");
+				alert.setContentText("패스워드는 숫자 4자리로 입력해주세요");
+				alert.showAndWait();
+			} else if (txtStoreName.getText().trim().equals("")) { // 매장명이 공백이면
+				txtPassword.clear(); // 비밀번호를 지워준다
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("매장 등록 실패");
+				alert.setHeaderText("매장명 미입력");
+				alert.setContentText("매장명을 입력해주세요");
+				alert.showAndWait();
+			} else if (txtStoreAddress.getText().trim().equals("")) { // 매장주소가 공백이면
+				txtPassword.clear(); // 비밀번호를 지워준다
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("매장 등록 실패");
+				alert.setHeaderText("매장주소 미입력");
+				alert.setContentText("매장주소를 입력해주세요");
+				alert.showAndWait();
+			} else if (txtStoreTel.getText().trim().equals("")) { // 매장번호가 공백이면
+				txtPassword.clear(); // 비밀번호를 지워준다
+
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("매장 등록 실패");
+				alert.setHeaderText("매장번호 미입력");
+				alert.setContentText("매장번호를 입력해주세요");
+				alert.showAndWait();
+			}
+
+			// 인스턴스 생성
+			StoreJoinVO sjvo = null;
+			StoreJoinDAO sjdao = null;
+			// 등록성공여부 판단변수
+			boolean joinSuccess = false;
+			// 인스턴스 생성및 (입력받은 값을 객체로 넣어준다)
+			if(txtPassword.getText().trim().length() <= 4 && !txtPassword.getText().trim().equals("")) {
+			sjvo = new StoreJoinVO(txtStoreCode.getText().trim(), txtStoreAddress.getText().trim(),
+					txtStoreName.getText().trim(), Integer.parseInt(txtPassword.getText().trim()),
+					Integer.parseInt(txtStoreTel.getText().trim()));
+			}
+			// 인스턴스 생성
+			sjdao = new StoreJoinDAO();
+			// 객체롤 매개변수로 메소드에 넣어주며 호출
+			joinSuccess = sjdao.getStoreRegiste(sjvo);
+
+			if (joinSuccess) {
+
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("알림");
+				alert.setHeaderText("매장등록 성공");
+				alert.setContentText("");
+				alert.showAndWait();
+				handlerBtnCancelAction(event);
+			}
+
+		}catch(NumberFormatException e) { 
+		System.out.println(e);
+	}catch (Exception e1) {
+			e1.printStackTrace();
+		}
+
+	}
+
+	// 등록창 닫기 메소드
+	public void handlerBtnCancelAction(ActionEvent event) {
+		Stage stage = (Stage) btnCancle.getScene().getWindow();
+		stage.close();
 	}
 
 }
