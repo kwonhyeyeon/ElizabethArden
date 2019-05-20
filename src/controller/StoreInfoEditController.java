98ec06789191b5decb0be814a3258dbc2b230bab
InSub Lee <dldlstjq1212@gmail.com>
2019-05-20 09:38:23 +0900

    ?

--- src/controller/StoreInfoEditController.java
+++ src/controller/StoreInfoEditController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
@@ -11,9 +12,11 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 import model.StoreVO;
 
@@ -73,27 +76,65 @@ public class StoreInfoEditController implements Initializable {
 		btnEdit.setOnAction(event -> handlerBtnEditAction(event)); // 변경 버튼 이벤트 메소드
 		btnCancle.setOnAction(event -> handlerBtnCancelAction(event)); // 수정창 닫기 이벤트 메소드
 
-		
 		// btnEdit.setDisable(true); // 변경 버튼 비활성화
-		
+
 	}
+
 	// 변경버튼 이벤트 메소드
 	public void handlerBtnEditAction(ActionEvent event) {
 		// TODO Auto-generated method stub
 		// sdao인스턴스 생성
 		StoreDAO sdao = new StoreDAO();
 		StoreVO svo = new StoreVO();
-		
-		
-		svo = new StoreVO(txtStoreAddress.getText().trim(), txtStoreName.getText().trim(), 
-				Integer.parseInt(txtPwEdit.getText().trim()), Integer.parseInt(txtStoreTel.getText().trim()));
-		
-		sdao.InfoEdit(svo);
-	
-	
-	
-	
+		try {
+			if (txtStoreName.getText().trim().equals("")) {
+				// 비밀번호 크기가 4보다 크거나 공백일 때
+
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("매장 정보 변경 실패");
+				alert.setHeaderText("매장명 미입력");
+				alert.setContentText("매장명을 입력하십시오");
+				alert.showAndWait();
+			} else if (txtPwEdit.getText().trim().length() > 4 || txtPwEdit.getText().trim().equals("")) { // 매장명이 공백이면
+
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("매장 정보 변경 실패");
+				alert.setHeaderText("패스워드입력 오류");
+				alert.setContentText("숫자 4자리로 입력하십시오");
+				alert.showAndWait();
+			} else if (txtStoreTel.getText().trim().equals("")) { // 매장주소가 공백이면
+
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("매장 정보 변경 실패");
+				alert.setHeaderText("매장번호 미입력");
+				alert.setContentText("매장번호를 입력해주세요");
+				alert.showAndWait();
+			} else if (txtStoreAddress.getText().trim().equals("")) { // 매장번호가 공백이면
+
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("매장 정보 변경 실패");
+				alert.setHeaderText("매장주소 미입력");
+				alert.setContentText("매장주소를 입력해주세요");
+				alert.showAndWait();
+			} else if (!(txtPwEdit.getText().trim().length() > 4 && txtPwEdit.getText().trim().equals(""))) {
+				svo = new StoreVO(txtStoreAddress.getText().trim(), txtStoreName.getText().trim(),
+						Integer.parseInt(txtPwEdit.getText().trim()), Integer.parseInt(txtStoreTel.getText().trim()));
+
+				sdao.InfoEdit(svo);
+
+				// 변경후 등록창닫기 이벤트 호출
+				handlerBtnCancelAction(event);
+			}
+
+		} catch (Exception e) {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("매장 정보 변경 실패");
+			alert.setHeaderText("비밀번호는 숫자 4자리로만 입력하십시오..");
+			alert.setContentText("");
+			alert.showAndWait();
+		}
 	}
+
 	// 수정창 닫기 이벤트 메소드
 	public void handlerBtnCancelAction(ActionEvent event) {
 		Stage stage = (Stage) btnCancle.getScene().getWindow();
