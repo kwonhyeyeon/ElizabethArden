3a745f4da07bef70ade3ebc761499858e99e4435
InSub Lee <dldlstjq1212@gmail.com>
2019-05-30 09:08:52 +0900

    ReturnTabController 복구

--- src/controller/ReturnTabController.java
+++ src/controller/ReturnTabController.java
@@ -227,7 +227,36 @@ public class ReturnTabController implements Initializable {
 	// 달력에서 클릭 선택 이벤트 핸들러
 	public void handlerdpDateAction(ActionEvent event) {
 		// TODO Auto-generated method stub
+		ReturnDAO rdao = new ReturnDAO();
+		ReturnVO rvo = new ReturnVO();
 
+		String selectedDay = dpDate.getValue().toString();
+
+		ArrayList<ReturnVO> list = null;
+		ReturnState.removeAll(ReturnState);
+
+		try {
+			list = rdao.getOrderDate(selectedDay);
+			int rowCount = list.size();
+
+			if (list.size() == 0) {
+				Alert alert;
+				alert = new Alert(AlertType.WARNING);
+				alert.setTitle("출고안내");
+				alert.setHeaderText("[ " + selectedDay + " ] 에 등록된 상품이 없습니다.");
+				alert.setContentText("");
+				// 경고창 크기설정 불가
+				alert.setResizable(false);
+				// 경고창을 보여주고 기다린다
+				alert.showAndWait();
+			} else {
+				for (int index = 0; index < rowCount; index++) {
+					rvo = list.get(index);
+					ReturnState.add(rvo);
+				}
+			}
+		} catch (Exception e) {
+		}
 	}
 
 	// 출고확인버튼 이벤트 핸들러
@@ -236,8 +265,6 @@ public class ReturnTabController implements Initializable {
 		ReturnVO rvo2 = new ReturnVO();
 		ReturnDAO rdao = new ReturnDAO();
 		rvo2 = tableReturnState.getSelectionModel().getSelectedItem();
-		String xy = tableReturnState.getSelectionModel().getSelectedItem().getBeReleased();
-		String selectedP_name = tableReturnState.getSelectionModel().getSelectedItem().getRp_name(); // 상품명
 
 		if (rvo2 == null) {
 			Alert alert;
@@ -249,28 +276,35 @@ public class ReturnTabController implements Initializable {
 			alert.setResizable(false);
 			// 경고창을 보여주고 기다린다
 			alert.showAndWait();
-		} else if (xy.equals("Y")) {
-
-			Alert alert;
-			alert = new Alert(AlertType.WARNING);
-			alert.setTitle("출고안내");
-			alert.setHeaderText(selectedP_name + "");
-			alert.setContentText("해당 상품은 이미 출고확인이 되었습니다.");
-			// 경고창 크기설정 불가
-			alert.setResizable(false);
-			// 경고창을 보여주고 기다린다
-			alert.showAndWait();
-
 		} else {
 
-			String selectedP_code = tableReturnState.getSelectionModel().getSelectedItem().getRp_code();
-			int selectedP_ea = tableReturnState.getSelectionModel().getSelectedItem().getRp_ea();
+			String xy = tableReturnState.getSelectionModel().getSelectedItem().getBeReleased();
+			String selectedP_name = tableReturnState.getSelectionModel().getSelectedItem().getRp_name(); // 상품명
 
-			rdao.SetY(today, selectedP_code);
-			rdao.setIn_Out(selectedP_ea, selectedP_code);
+			if (xy.equals("Y")) {
 
-			ReturnStateTotalList();
-			productTotalList();
+				Alert alert;
+				alert = new Alert(AlertType.WARNING);
+				alert.setTitle("출고안내");
+				alert.setHeaderText(selectedP_name + "");
+				alert.setContentText("해당 상품은 이미 출고확인이 되었습니다.");
+				// 경고창 크기설정 불가
+				alert.setResizable(false);
+				// 경고창을 보여주고 기다린다
+				alert.showAndWait();
+
+			} else {
+
+				String selectedP_code = tableReturnState.getSelectionModel().getSelectedItem().getRp_code();
+				int selectedP_ea = tableReturnState.getSelectionModel().getSelectedItem().getRp_ea();
+
+				rdao.SetY(today, selectedP_code);
+				rdao.setIn_Out(selectedP_ea, selectedP_code);
+
+				ReturnStateTotalList();
+				productTotalList();
+
+			}
 			try {
 
 			} catch (Exception e) {
